e8818a91e64a976f0c0e167fd100e9c7a02ed187
Tyler Lubeck <tyler@tylerlubeck.com>
2015-02-07 16:32:50 -0500

    Working camera view

--- HackAtBrown2015/MainWindow.xaml.cs
+++ HackAtBrown2015/MainWindow.xaml.cs
@@ -18,38 +18,47 @@ namespace HackAtBrown2015 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+
+    enum MODE { Color, Depth, Infrared };
     public partial class MainWindow : Window {
         KinectSensor _sensor;
         MultiSourceFrameReader _reader;
+        MODE type;
+        
         public MainWindow() {
             InitializeComponent();
+            type = MODE.Color;
+
+            _sensor = KinectSensor.GetDefault();
 
             if (_sensor != null) {
+                Console.WriteLine("SENSOR IS NOT NULL");
                 _sensor.Open();
+                _reader = _sensor.OpenMultiSourceFrameReader(FrameSourceTypes.Color | FrameSourceTypes.Infrared | FrameSourceTypes.Depth);
+                _reader.MultiSourceFrameArrived += Reader_MultiSourceFrameArrived;
             }
 
-            _reader = _sensor.OpenMultiSourceFrameReader(FrameSourceTypes.Color | FrameSourceTypes.Infrared | FrameSourceTypes.Depth);
-            _reader.MultiSourceFrameArrived += Reader_MultiSourceFrameArrived;
         }
 
         void Reader_MultiSourceFrameArrived(object sender, MultiSourceFrameArrivedEventArgs e) {
             var reference = e.FrameReference.AcquireFrame();
 
             using (var frame = reference.ColorFrameReference.AcquireFrame()) {
-                if (frame != null) {
+                Console.WriteLine("HERE");
+                if (frame != null && type == MODE.Color) {
                     camera.Source = ToBitmap(frame);
                 }
             }
             
             using (var frame = reference.DepthFrameReference.AcquireFrame()) {
-                if (frame != null) {
-
+                if (frame != null && type == MODE.Depth) {
+                    camera.Source = ToBitmap(frame);
                 }
             }
 
             using (var frame = reference.InfraredFrameReference.AcquireFrame()) {
-                if (frame != null) {
-
+                if (frame != null && type == MODE.Infrared) {
+                    camera.Source = ToBitmap(frame);
                 }
             }
         }
@@ -73,6 +82,75 @@ namespace HackAtBrown2015 {
             int stride = width * format.BitsPerPixel / 8;
 
             return BitmapSource.Create(width, height, 96, 96, format, null, pixels, stride);
+        }
+
+        private ImageSource ToBitmap(DepthFrame frame) {
+            int width = frame.FrameDescription.Width;
+            int height = frame.FrameDescription.Height;
+            PixelFormat format = PixelFormats.Bgr32;
+
+            ushort minDepth = frame.DepthMinReliableDistance;
+            ushort maxDepth = frame.DepthMaxReliableDistance;
+
+            ushort[] depthData = new ushort[width * height];
+            byte[] pixelData = new byte[width * height * (PixelFormats.Bgr32.BitsPerPixel + 7) / 8];
+
+            frame.CopyFrameDataToArray(depthData);
+
+            int colorIndex = 0;
+            for (int depthIndex = 0; depthIndex < depthData.Length; ++depthIndex) {
+                ushort depth = depthData[depthIndex];
+                byte intensity = (byte)(depth >= minDepth && depth <= maxDepth ? depth : 0);
+
+                pixelData[colorIndex++] = intensity; // Blue
+                pixelData[colorIndex++] = intensity; // Green
+                pixelData[colorIndex++] = intensity; // Red
+
+                ++colorIndex;
+            }
+
+            int stride = width * format.BitsPerPixel / 8;
+
+            return BitmapSource.Create(width, height, 96, 96, format, null, pixelData, stride);
+        }
+
+        private ImageSource ToBitmap(InfraredFrame frame) {
+            int width = frame.FrameDescription.Width;
+            int height = frame.FrameDescription.Height;
+            PixelFormat format = PixelFormats.Bgr32;
+
+            ushort[] infraredData = new ushort[width * height];
+            byte[] pixelData = new byte[width * height * (PixelFormats.Bgr32.BitsPerPixel + 7) / 8];
+
+            frame.CopyFrameDataToArray(infraredData);
+
+            int colorIndex = 0;
+            for (int infraredIndex = 0; infraredIndex < infraredData.Length; ++infraredIndex) {
+                ushort ir = infraredData[infraredIndex];
+                byte intensity = (byte)(ir >> 8);
+
+                pixelData[colorIndex++] = intensity; // Blue
+                pixelData[colorIndex++] = intensity; // Green   
+                pixelData[colorIndex++] = intensity; // Red
+
+                ++colorIndex;
+            }
+
+            int stride = width * format.BitsPerPixel / 8;
+
+            return BitmapSource.Create(width, height, 96, 96, format, null, pixelData, stride);
+        }
+
+        private void Color_Click(object sender, RoutedEventArgs e) {
+            type = MODE.Color;
+        }
+
+        private void Depth_Click(object sender, RoutedEventArgs e) {
+            type = MODE.Depth;
+        }
+
+        private void Infrared_Click(object sender, RoutedEventArgs e) {
+            type = MODE.Infrared;
         }
 
 

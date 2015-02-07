5a9026cb9d9351097122e1cfd1dce78eb887e861
Tyler Lubeck <tyler@tylerlubeck.com>
2015-02-07 16:16:30 -0500

    Initial Commit

---
+++ HackAtBrown2015/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Kinect;
+
+namespace HackAtBrown2015 {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        KinectSensor _sensor;
+        MultiSourceFrameReader _reader;
+        public MainWindow() {
+            InitializeComponent();
+
+            if (_sensor != null) {
+                _sensor.Open();
+            }
+
+            _reader = _sensor.OpenMultiSourceFrameReader(FrameSourceTypes.Color | FrameSourceTypes.Infrared | FrameSourceTypes.Depth);
+            _reader.MultiSourceFrameArrived += Reader_MultiSourceFrameArrived;
+        }
+
+        void Reader_MultiSourceFrameArrived(object sender, MultiSourceFrameArrivedEventArgs e) {
+            var reference = e.FrameReference.AcquireFrame();
+
+            using (var frame = reference.ColorFrameReference.AcquireFrame()) {
+                if (frame != null) {
+                    camera.Source = ToBitmap(frame);
+                }
+            }
+            
+            using (var frame = reference.DepthFrameReference.AcquireFrame()) {
+                if (frame != null) {
+
+                }
+            }
+
+            using (var frame = reference.InfraredFrameReference.AcquireFrame()) {
+                if (frame != null) {
+
+                }
+            }
+        }
+
+        private ImageSource ToBitmap(ColorFrame frame) {
+            int width = frame.FrameDescription.Width;
+            int height = frame.FrameDescription.Height;
+            PixelFormat format = PixelFormats.Bgr32;
+
+            byte[] pixels = new byte[width * height * ((PixelFormats.Bgr32.BitsPerPixel + 7) / 8)];
+
+            if (frame.RawColorImageFormat == ColorImageFormat.Bgra)
+            {
+                frame.CopyRawFrameDataToArray(pixels);
+            }
+            else
+            {
+                frame.CopyConvertedFrameDataToArray(pixels, ColorImageFormat.Bgra);
+            }
+
+            int stride = width * format.BitsPerPixel / 8;
+
+            return BitmapSource.Create(width, height, 96, 96, format, null, pixels, stride);
+        }
+
+
+    }
+
+}
